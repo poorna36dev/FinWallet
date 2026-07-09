e7b9bf1df2f1f05c6255b293797ceefc95648918
poorna36dev <poorna2429@gmail.com>
2026-07-09 10:31:26 +0530

    properties added

--- fintech/src/test/java/com/poorna/fintech/FintechApplicationTests.java
+++ fintech/src/test/java/com/poorna/fintech/FintechApplicationTests.java
@@ -2,8 +2,10 @@ package com.poorna.fintech;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FintechApplicationTests {
 
 	@Test
